41019c69bf29a8c3999376c8f13dba063ac377e0
lixiaomeng <lixiaomeng8520@163.com>
2019-01-16 10:26:22 +0800

    gin test

--- gin.go
+++ gin.go
@@ -3,32 +3,25 @@ package main
 import (
 	"net/http"
 	"github.com/gin-gonic/gin"
+	"fmt"
 )
 
 func gin1() {
 	r := gin.Default()
 
-	r.GET("/user/age", userAge)
+	v1 := r.Group("/v1")
+	v1.GET("/login", loginEndpoint)
 
-	r.GET("/user/name", userName)
-
-	r.GET("/book", book)
-
-	r.Run()
+	v2 := r.Group("/v1")
+	
+	fmt.Println(v2)
+	r.Run(":80")
 }
 
 func test() {
 
 }
 
-func userAge(c *gin.Context) {
-	c.String(http.StatusOK, "/user/age")
-}
-
-func userName(c *gin.Context) {
-	c.String(http.StatusOK, "/user/name")
-}
-
-func book(c *gin.Context) {
-	c.String(http.StatusOK, "/book")
+func loginEndpoint(c *gin.Context) {
+	c.String(http.StatusOK, "loginEndpoint")
 }
--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 package main
 
 func main() {
-	sync1()
+	gin1()
 }
 
