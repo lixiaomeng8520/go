1e009b4612e855cb4cc485f3027c255cbcfafa9f
lixiaomeng <lixiaomeng8520@163.com>
2019-08-20 19:08:51 +0800

    * ss proxy

--- main.go
+++ main.go
@@ -1,4 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net"
+	"io/ioutil"
+	// "reflect"
+)
+
 func main() {
+	// l : *TCPListener
+	l, err := net.Listen("tcp", "0.0.0.0:10000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	for {
+		// c : *TCPConn
+		c, err := l.Accept()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		b, err := ioutil.ReadAll(c)
+		fmt.Println(string(b), err)
+	}
 }
